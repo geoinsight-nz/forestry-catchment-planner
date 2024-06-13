1bdf2b9c45ec11a87df9849ce692791bddc2fce6
Henry Babbage <hbabbage@gmail.com>
2024-06-13 23:16:04 +0200

    add tailwind css typography

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
@@ -126,7 +126,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 } satisfies Config;
 
-export default config
+export default config;
