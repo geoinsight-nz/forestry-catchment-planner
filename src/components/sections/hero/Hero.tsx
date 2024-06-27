91534bb2372f0ea29fed5e1f1af13d910b939b0e
Henry Babbage <hbabbage@gmail.com>
2024-06-27 22:27:13 +0200

    get full screen mockup into landing hero

--- src/components/sections/hero/Hero.tsx
+++ src/components/sections/hero/Hero.tsx
@@ -3,20 +3,20 @@
 import Image from "next/image";
 
 import ipad from "/public/images/mockups/ipad_pro_1.png";
-import monitor from "/public/images/mockups/monitor_1.png";
+import mac from "/public/images/mockups/pro_display_xdr_mockup_2x.png";
 
 export default function Hero() {
   return (
     <div className="relative mt-[6rem] animate-fade-up opacity-0 [--animation-delay:400ms] [perspective:2000px] after:absolute after:inset-0 after:z-50">
       <div className="relative mx-auto h-full max-w-4xl">
         <Image
-          src={monitor}
+          src={mac}
           alt="FCP"
           sizes="100vw"
           className="relative hidden h-full w-auto rounded-[inherit] object-contain dark:block"
         />
         <Image
-          src={monitor}
+          src={mac}
           alt="FCP"
           sizes="100vw"
           className="relative block h-full w-auto rounded-[inherit] object-contain dark:hidden"
