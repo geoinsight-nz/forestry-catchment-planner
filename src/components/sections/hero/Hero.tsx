db8c736c1ac856fcdd7e14482d53b44945e7f6a9
Henry Babbage <hbabbage@gmail.com>
2024-06-28 03:29:37 +0200

    fix error with height and width on hero

--- src/components/sections/hero/Hero.tsx
+++ src/components/sections/hero/Hero.tsx
@@ -8,24 +8,24 @@ import mac from "/public/images/mockups/pro_display_xdr_mockup_2x.png";
 export default function Hero() {
   return (
     <div className="relative mt-[6rem] animate-fade-up opacity-0 [--animation-delay:400ms] [perspective:2000px] after:absolute after:inset-0 after:z-50">
-      <div className="relative mx-auto h-full max-w-4xl">
+      <div className="relative mx-auto h-fit max-w-4xl">
         <Image
           src={mac}
           alt="FCP"
           sizes="100vw"
-          className="relative hidden h-full w-auto rounded-[inherit] object-contain dark:block"
+          className="relative hidden h-auto w-full rounded-[inherit] object-contain dark:block"
         />
         <Image
           src={mac}
           alt="FCP"
           sizes="100vw"
-          className="relative block h-full w-auto rounded-[inherit] object-contain dark:hidden"
+          className="relative block h-auto w-full rounded-[inherit] object-contain dark:hidden"
         />
         <Image
           src={ipad}
           alt="iPad"
           sizes="100vw"
-          className="absolute bottom-0 right-0 max-w-[32vw] translate-x-[2rem] px-4 brightness-95 md:max-w-[24vw]"
+          className="absolute bottom-0 h-auto right-0 max-w-[32vw] translate-x-[2rem] px-4 brightness-95 md:max-w-[24vw]"
         />
       </div>
     </div>
