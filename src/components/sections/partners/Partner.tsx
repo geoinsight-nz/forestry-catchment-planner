ecd2d43c21223ba74cd34b5c370935493d97fc1f
Henry Babbage <hbabbage@gmail.com>
2024-06-28 03:35:54 +0200

    fix partners darkmode

--- src/components/sections/partners/Partner.tsx
+++ src/components/sections/partners/Partner.tsx
@@ -14,13 +14,15 @@ export default function Partner({ title, about, body }: PartnerProps) {
     <div className="group flex w-full items-center justify-between border-t border-solid border-t-[var(--foreground)] py-[48px] transition last-of-type:border-b last-of-type:border-solid last-of-type:border-b-[var(--foreground)]">
       <div className="flex flex-col gap-6 transition duration-300">
         <div className="flex gap-4">
-          <h2 className="text-xl font-normal">{title}</h2>
+          <h2 className="text-xl font-normal text-brand-950 dark:text-brand-50">
+            {title}
+          </h2>
         </div>
         <div className="relative flex flex-col gap-4">
-          <p className="max-w-prose text-base text-brand-50 dark:text-brand-50">
+          <p className="max-w-prose text-base text-brand-950 dark:text-brand-50">
             {about}
           </p>
-          <p className="max-w-prose text-base text-brand-50 dark:text-brand-50">
+          <p className="max-w-prose text-base text-brand-950  dark:text-brand-50">
             {body}
           </p>
         </div>
