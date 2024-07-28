b30c2e94cbddf38665821b15029fc0114870894c
Henry Babbage <hbabbage@gmail.com>
2024-07-28 23:56:21 +0200

    comment out list mdx

--- src/components/sections/4/Features.tsx
+++ src/components/sections/4/Features.tsx
@@ -1,21 +1,21 @@
-import fs from "fs";
-import path from "path";
-import { Suspense } from "react";
+// import fs from "fs";
+// import path from "path";
+// import { Suspense } from "react";
 
-import { FeaturesListMDX } from "./FeaturesListMDX";
+// import { FeaturesListMDX } from "./FeaturesListMDX";
 
-export function Features() {
-  // MDX text file for this component
-  const filePath = path.join(
-    process.cwd(),
-    "src/app/_mdx-content/sections/4/list",
-    "list.mdx",
-  );
-  // Read the contents of the MDX file
-  const src = fs.readFileSync(filePath, "utf8");
-  return (
-    <Suspense>
-      <FeaturesListMDX source={src} />
-    </Suspense>
-  );
-}
+// export function Features() {
+//   // MDX text file for this component
+//   const filePath = path.join(
+//     process.cwd(),
+//     "src/app/_mdx-content/sections/4/list",
+//     "list.mdx",
+//   );
+//   // Read the contents of the MDX file
+//   const src = fs.readFileSync(filePath, "utf8");
+//   return (
+//     <Suspense>
+//       <FeaturesListMDX source={src} />
+//     </Suspense>
+//   );
+// }
