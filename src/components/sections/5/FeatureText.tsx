977865e50f6ad3ec3a2ce50c4ccf0bf7e09d75fd
Henry Babbage <hbabbage@gmail.com>
2024-09-03 19:53:03 +0200

    make transition clearer

--- src/components/sections/5/FeatureText.tsx
+++ src/components/sections/5/FeatureText.tsx
@@ -10,9 +10,9 @@ import { type Feature } from "./FeaturesList";
 type FeatureText = Omit<Feature, "image">;
 
 export default function FeatureText({ id, heading, text }: FeatureText) {
-  const ref = useRef<HTMLParagraphElement>(null);
+  const ref = useRef<HTMLDivElement>(null);
   const isInView = useInView(ref, {
-    margin: "-50% 0px -50% 0px",
+    margin: "-50% 0% -50% 0%",
   });
   const setInViewFeature = useFeatureStore((state) => state.setInViewFeature);
   const inViewFeature = useFeatureStore((state) => state.inViewFeature);
@@ -22,24 +22,25 @@ export default function FeatureText({ id, heading, text }: FeatureText) {
   }, [isInView, id, setInViewFeature, inViewFeature]);
 
   return (
-    <Prose className="py-16">
-      <h2
-        className={cn(
-          isInView ? "opacity-100" : "opacity-80",
-          "text-balance text-xl font-medium text-brand-950 transition-colors dark:text-brand-50 lg:text-3xl",
-        )}
-      >
-        {heading}
-      </h2>
-      <p
-        ref={ref}
-        className={cn(
-          isInView ? "opacity-100" : "opacity-80",
-          "pt-6 text-sm font-normal text-brand-950 transition-colors dark:text-brand-50 lg:text-base",
-        )}
-      >
-        {text}
-      </p>
-    </Prose>
+    <div ref={ref}>
+      <Prose className="py-16">
+        <h2
+          className={cn(
+            isInView ? "opacity-100" : "opacity-30",
+            "text-balance text-xl font-medium text-brand-950 transition-colors dark:text-brand-50 lg:text-3xl",
+          )}
+        >
+          {heading}
+        </h2>
+        <p
+          className={cn(
+            isInView ? "opacity-100" : "opacity-30",
+            "pt-6 text-sm font-normal text-brand-950 transition-colors dark:text-brand-50 lg:text-base",
+          )}
+        >
+          {text}
+        </p>
+      </Prose>
+    </div>
   );
 }
