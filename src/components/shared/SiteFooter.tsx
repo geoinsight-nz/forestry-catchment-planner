8892d4e2e135b5b0e029d8dd02d92d2c9b30018e
Henry Babbage <hbabbage@gmail.com>
2024-09-03 18:22:18 +0200

    improve footer

--- src/components/shared/SiteFooter.tsx
+++ src/components/shared/SiteFooter.tsx
@@ -1,20 +1,34 @@
 import Link from "next/link";
 
-const footerNavs = [
+type FooterNavItem = {
+  href: string;
+  name: string;
+  link: boolean;
+};
+
+type FooterNavSet = {
+  label: string;
+  items: FooterNavItem[];
+};
+
+const footerNavs: FooterNavSet[] = [
   {
-    label: "Product",
+    label: "Tool",
     items: [
       {
         href: "https://www.docs.forestrycatchmentplanner.nz",
         name: "Documentation",
+        link: true,
       },
       {
         href: "https://catchment.maphq.co.nz/",
         name: "Application",
+        link: true,
       },
       {
         href: "mailto:info@geoinsight.co.nz",
         name: "Contact",
+        link: true,
       },
     ],
   },
@@ -24,18 +38,22 @@ const footerNavs = [
       {
         href: "https://www.geoinsight.co.nz",
         name: "GeoInsight",
+        link: true,
       },
       {
         href: "https://induforgroup.com",
         name: "Indufor",
+        link: true,
       },
       {
         href: "https://www.gns.cri.nz",
         name: "GNS Science",
+        link: true,
       },
       {
         href: "https://www.canterbury.ac.nz/study/academic-study/engineering/schools-and-departments-engineering-forestry-product-design/school-of-forestry",
         name: "The University of Canterbury",
+        link: true,
       },
     ],
   },
@@ -44,20 +62,37 @@ const footerNavs = [
     items: [
       {
         href: "",
-        name: "Design: Blaine Western and Henry Babbage",
+        name: "Design:",
+        link: false,
+      },
+      {
+        href: "",
+        name: "Blaine Western",
+        link: false,
+      },
+      {
+        href: "",
+        name: "Henry Babbage",
+        link: false,
+      },
+      {
+        href: "",
+        name: "Programming:",
+        link: false,
       },
       {
         href: "https://www.github.com/henrybabbage",
-        name: "Programming: Henry Babbage",
+        name: "Henry Babbage",
+        link: true,
       },
     ],
   },
-];
+] as const;
 
 export function SiteFooter() {
   return (
     <footer className="">
-      <div className="w-full max-w-screen xl:pb-2">
+      <div className="max-w-screen w-full xl:pb-2">
         <div className="gap-4 p-4 px-6 py-16 sm:pb-16 md:flex md:justify-between">
           <div className="mb-12 flex flex-col gap-4">
             <Link
@@ -74,21 +109,30 @@ export function SiteFooter() {
               Proactive forest management for the future.
             </p>
           </div>
-          <div className="grid grid-cols-1 gap-8 sm:grid-cols-3 sm:gap-10 sm:max-w-4xl">
-            {footerNavs.map((nav) => (
+          <div className="grid grid-cols-1 gap-8 sm:max-w-4xl sm:grid-cols-3 sm:gap-10">
+            {footerNavs.map((nav: FooterNavSet) => (
               <div key={nav.label}>
                 <h2 className="mb-6 text-sm font-medium uppercase tracking-tighter text-gray-900 dark:text-white">
                   {nav.label}
                 </h2>
                 <ul className="grid gap-2">
-                  {nav.items.map((item) => (
-                    <li key={item.name}>
-                      <Link
-                        href={item.href}
-                        className="cursor-pointer text-sm font-[450] text-gray-400 duration-200 hover:text-gray-200"
-                      >
-                        {item.name}
-                      </Link>
+                  {nav.items.map((item: FooterNavItem) => (
+                    <li
+                      key={item.name}
+                      className="flex h-full w-full items-center"
+                    >
+                      {item.link === true ? (
+                        <Link
+                          href={item.href}
+                          className="cursor-pointer text-sm font-[450] text-gray-400 duration-200 hover:text-gray-200"
+                        >
+                          {item.name}
+                        </Link>
+                      ) : (
+                        <h3 className="flex h-full text-sm font-[450] text-gray-400">
+                          {item.name}
+                        </h3>
+                      )}
                     </li>
                   ))}
                 </ul>
