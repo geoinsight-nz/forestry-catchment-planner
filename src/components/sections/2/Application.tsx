17082ebb10eccc08e9d2a8aacfcc0a1779b41e47
Henry Babbage <hbabbage@gmail.com>
2024-09-03 18:51:05 +0200

    fix column alignment by setting max-w-screen-2xl for section

--- src/components/sections/2/Application.tsx
+++ src/components/sections/2/Application.tsx
@@ -4,7 +4,7 @@ import trees from "/public/images/forests/forest_trees_duotone.png";
 
 export default function Application() {
   return (
-    <section id="intro" className="flex items-center justify-center">
+    <section id="intro" className="mx-auto flex max-w-screen-2xl">
       <div className="grid h-full grid-cols-1 bg-background pb-14 dark:bg-brand-950 md:h-full md:grid-cols-2 md:pb-0">
         <div className="relative order-2 h-96 w-full md:order-1 md:h-full">
           <Image
--- src/components/sections/3/Context.tsx
+++ src/components/sections/3/Context.tsx
@@ -4,7 +4,7 @@ import forest from "/public/images/forests/forest_river_duotone.png";
 
 export default function Context() {
   return (
-    <section id="context" className="flex items-center justify-center">
+    <section id="context" className="mx-auto flex max-w-screen-2xl">
       <div className="grid h-full grid-cols-1 bg-background pb-14 dark:bg-brand-950 md:h-full md:grid-cols-2 md:pb-0">
         <div className="order-2 h-fit px-6 py-6 md:order-2 md:h-full md:px-14 md:py-14">
           <ContextText />
